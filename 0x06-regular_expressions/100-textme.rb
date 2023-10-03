ff10c830dacfaaa9d31f3bb3639fa7fc20baf9c3
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-10-03 17:43:44 +0100

    regex

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join
+
+match = ARGV[0].match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+
+if match
+  from = match[1]
+  to = match[2]
+  flags = match[3]
+  puts "#{from},#{to},#{flags}"
+end
