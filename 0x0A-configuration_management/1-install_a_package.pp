699bfec175a7170143b6a64913a4032bd84fec17
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-27 12:30:46 +0100

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# installation of flask 2.1.0  using  script
-package { 'flusk-2.1.0':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+# install version from pip3
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==2.0.2',
+  path    => '/usr/local/bin',
+  require => Package['python3-pip'],
 }
