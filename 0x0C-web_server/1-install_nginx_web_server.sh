16fd9fe39b25d0df1d8975e80a190fce529db6dd
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-28 16:24:47 +0100

    install

--- 0x0C-web_server/1-install_nginx_web_server.sh
+++ 0x0C-web_server/1-install_nginx_web_server.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # installing ngnix server and restarting it
 
-apt-get update
+sudo pt-get update
 
-apt-get -y install nginx
+sudo apt-get -y install nginx
 
-sudo echo "Hello World!" > /var/www/html/index.html
+sudo sh -c 'echo "Hello World!" > /var/www/html/index.html'
 
 sudo service nginx reload
