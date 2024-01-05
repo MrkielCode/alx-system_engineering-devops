80baaa9afd773ed899e43f4622ade42e9fd131fd
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-01-05 20:03:29 +0100

    gather user

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+script to get the users data and print out
+the completed task
+"""
+from sys import argv
+import requests
+
+
+def get_user(id):
+    """ get the user data"""
+    api_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+
+
+def todo_done():
+    """get the task done """
+    id = argv[1]
+    username = get_user(id).get('name')
+    user_id = get_user(id).get('id')
+
+    api_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        todo = response.json()
+        total_task = len(todo)
+    completed_todo = [task for task in todo if task.get('completed')]
+    completed = len(completed_todo)
+
+    print(f"Employee {username} is done with tasks "
+          f"({completed}/{total_task}):")
+    for todo in completed_todo:
+        print('\t', todo['title'])
+
+
+if __name__ == "__main__":
+    todo_done()
