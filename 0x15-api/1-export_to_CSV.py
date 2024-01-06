d3d279dae59ef13f8a0ac23edfdbd92d57dcd1bd
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-01-06 15:41:40 +0100

    export csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+script to get the users data and print out
+the completed task
+"""
+import requests
+import csv
+from sys import argv
+
+
+def get_user(id):
+    """ get the user data"""
+    api_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+
+
+def export_to_csv(user_id, username, todo):
+    """ exporting to json file """
+    filename = f"{user_id}.csv"
+    with open(filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_ALL)
+        for task in todo:
+            csv_writer.writerow([
+                user_id, username,
+                str(task['completed']),
+                task['title']])
+
+
+def todo_done():
+    """get the task done """
+    id = argv[1]
+    username = get_user(id).get('name')
+    user_id = get_user(id).get('id')
+
+    api_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        todo = response.json()
+    export_to_csv(user_id, username, todo)
+
+
+if __name__ == "__main__":
+    todo_done()
