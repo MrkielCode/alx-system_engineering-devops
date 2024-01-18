65143d4a8b1528084fbf3a0fef2481d25bf359a7
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-01-18 19:59:03 +0100

    fix error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ getting number of subscribers """
+
     headers = {'User-Agent': 'MyRedditBot/1.0'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    if response.status_code == 404:
+        return 0
+    else:
         json_response = response.json()
         subscribers = json_response['data']['subscribers']
         return subscribers
-    else:
-        return 0
