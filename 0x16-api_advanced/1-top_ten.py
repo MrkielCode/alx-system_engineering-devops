cfefb13fec4e7d819d403873cc5a99fe803e57d8
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-01-19 15:34:57 +0100

    update top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,23 +6,18 @@ import requests
 
 
 def top_ten(subreddit):
-    """ Getting the top host post from subreddits"""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    params = {'limit': 10}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
-    response = requests.get(url,
-                            headers=headers,
-                            params=params,
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    if response.status_code != 200:
-        print('None')
+    if response.status_code == 404:
+        print("None")
         return
-    else:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
